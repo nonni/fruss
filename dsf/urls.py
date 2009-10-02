aa3261a6fe0d40c7bce5d5bfccfd167882409d0d
Nonni <nonnsi@gmail.com>
2009-10-02 21:55:50 +0000

    Pagination implemented for thread list

--- dsf/urls.py
+++ dsf/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls.defaults import *
 from models import Thread
 import views
 
-queryset = {'queryset': Thread.objects.order_by('pub_date')}
+queryset = {'queryset': Thread.objects.order_by('-pub_date'),
+        'paginate_by': 5, 
+        }
+
+
 urlpatterns = patterns('django.views.generic.list_detail',
         url('^$', 'object_list', queryset, name='posts'),
         url('^(?P<object_id>\d+)/$', 'object_detail', queryset, name='thread'),
