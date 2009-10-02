8e6de8d23fee8c9a2017cb461c90d11a540a3d03
Nonni <nonnsi@gmail.com>
2009-10-02 22:24:40 +0000

    Paginating

--- dsf/urls.py
+++ dsf/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls.defaults import *
 from models import Thread
 import views
 
-queryset = {'queryset': Thread.objects.order_by('-pub_date'),
-        'paginate_by': 5, 
-        }
+thread_list = {'queryset': Thread.objects.order_by('-pub_date'),
+        'paginate_by': 5}
 
+thread_detail = {'queryset': Thread.objects.all()}
 
 urlpatterns = patterns('django.views.generic.list_detail',
-        url('^$', 'object_list', queryset, name='posts'),
-        url('^(?P<object_id>\d+)/$', 'object_detail', queryset, name='thread'),
+        url('^$', 'object_list', thread_list, name='posts'),
+        url('^(?P<object_id>\d+)/$', 'object_detail', thread_detail, name='thread'),
         url('^new/$', views.new_thread),
 )
